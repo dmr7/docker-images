20117efb205af4d3a54c37a1bb81700067158a68
protomouse <root@protomou.se>
2016-01-29 13:07:56 +0100

    logspout: use kinesis

--- logspout/modules.go
+++ logspout/modules.go
@@ -1,6 +1,7 @@
 package main
 
 import (
-  _ "github.com/looplab/logspout-logstash"
-  _ "github.com/gliderlabs/logspout/transports/udp"
+  _ "github.com/gliderlabs/logspout/httpstream"
+  _ "github.com/gliderlabs/logspout/routesapi"
+  _ "github.com/remind101/logspout-kinesis"
 )
